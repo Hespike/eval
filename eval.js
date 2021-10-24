68020c9c7da93f966edcce93e1fd4b233d4f6c67
Sárdi András <sardi.andras@gmail.com>
2021-10-24 14:10:29 +0200

    Create eval.js

---
+++ eval.js
@@ -0,0 +1,4 @@
+var x=5, y=6;
+
+console.log(eval(x*y))
+console.log(eval("x * y")); // az eval végrehajta az argumentet akár string formában is, a 3. és 4. sor kimenete is a konzolra kiírt 30 lesz. ggez
